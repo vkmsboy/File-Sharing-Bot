d627e44bff87620d896656f83cf58811b46a0c3d
vkmsboy <95024188+vkmsboy@users.noreply.github.com>
2022-03-04 15:19:43 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -92,11 +92,11 @@ async def start_command(client: Client, message: Message):
                 [
                     InlineKeyboardButton("😊 About Me", callback_data = "about"),
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
+                ]
                 [
-                    
                     InlineKeyboardButton("Movie Group", url="https//t.me/Chat_Room_y")
                 ]
-                ]
+                
             ]
         )
         await message.reply_text(
